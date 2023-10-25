4f3d2d62958dc8ec02afeedc15a4ec07d0c14118
Octavian <octavian.hagi@gmail.com>
2023-10-25 15:01:04 -0600

    updated .gitignore

--- a.py
+++ a.py
@@ -1,7 +1,8 @@
 print('Opening a txt file for writing')
 f_obj = open('test.txt', mode='w')
 print('Opening a txt file for reading')
-f_obj = open('test.txt', mode='r')
+f_obj = open('test.txt', mode='r') # overwrites writing mode
+# f_obj.close()   #make sure to close files      # look at error below
 
 # uncomment below, will result in error
 # f_obj.write("hi")
